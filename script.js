66c6050bd036106d5e1e82787e2415088d48f00b
Budás Norbert <budasnorbi@niif.hu>
2019-07-22 15:04:41 +0200

    cv update

--- script.js
+++ script.js
@@ -12,15 +12,61 @@ window.addEventListener('load', e => {
     rootMargin: '0px',
     threshold: 1,
   }
+
+  const getPct = stage => {
+    switch(stage) {
+      case 'one': {
+        return 20;
+      }
+      case 'two': {
+        return 40;
+      }
+      case 'three': {
+        return 60;
+      }
+      case 'four': {
+        return 80;
+      }
+    }
+  };
+
+  const getLanguagePct = stage => {
+    switch(stage) {
+      case 'one': {
+        return 100 / 6 * 1;
+      }
+      case 'two': {
+        return 100 / 6 * 2;
+      }
+      case 'three': {
+        return 100 / 6 * 3;
+      }
+      case 'four': {
+        return 100 / 6 * 4;
+      }
+      case 'five': {
+        return 100 / 6 * 5;
+      }
+    }
+  };
   
   const observer = new IntersectionObserver( entries => {
     entries
       .filter( entry => entry.isIntersecting)
       .forEach( observerEntry => {
-        if(observerEntry.target.style.width === '0%'){
-          const expPct = observerEntry.target.getAttribute('data-expereience-pct');
-          observerEntry.target.style.width = expPct + '%';
+        console.log(observerEntry.target.className)
+        if(observerEntry.target.className === 'exped exped-left'){
+          if(observerEntry.target.style.width === '0%'){
+            const stage = observerEntry.target.getAttribute('data-expereience-stage');
+            observerEntry.target.style.width = getLanguagePct(stage) + '%';
+          }
+        } else {
+          if(observerEntry.target.style.width === '0%'){
+            const stage = observerEntry.target.getAttribute('data-expereience-stage');
+            observerEntry.target.style.width = getPct(stage) + '%';
+          }
         }
+
       });
   }, options);
 
